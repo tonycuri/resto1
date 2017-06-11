8086e1262e818fc8ac5fbcb3031bf9f4c2b0703e
tony <curicaballero@gmail.com>
2017-06-11 20:19:08 -0300

    comenzamos el formulario

---
+++ js/steps.js
@@ -0,0 +1,22 @@
+;(function(){
+  const selector = "#contact-form"
+
+  //agarra el formulario y mete los metodos de jquery
+  $(selector).find(".input").on("change",()=>{
+    console.log("cambie de valor")
+  })
+  //Helpers
+
+  function validar_formulario(){
+
+  }
+
+  function es_valido_formulario(){
+
+  }
+
+  function enfocar_nuevo_paso(){
+
+  }
+
+})()
