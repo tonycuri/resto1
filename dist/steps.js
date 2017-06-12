76bfdf5f525a895355a220f90335c2c9d5f8ddee
tony <curicaballero@gmail.com>
2017-06-11 23:16:54 -0300

    form

--- dist/steps.js
+++ dist/steps.js
@@ -1,1 +1,1 @@
-"use strict";!function(){function n(n){n.focus()}$("#contact-form").find(".input").on("change",function(o){n($(o.target).next()),console.log($el),console.log("cambie de valor")})}();
+"use strict";!function(){function t(){n()||e($(i).find(".input:invalid").first().parent())}function n(){return document.querySelector(i).checkValidity()}function e(t){$(".step.active").removeClass("active"),t.addClass("active"),t.find(".input").focus()}var i="#contact-form";$(i).find(".input").on("change",function(n){var i=$(n.target).parent().next(".step");i.length>0?e(i):t()})}();
--- js/steps.js
+++ js/steps.js
@@ -1,30 +1,42 @@
 ;(function(){
+
+  // $(".step:nth-child(1)").addClass("active")
   const selector = "#contact-form"
 
   //agarra el formulario y mete los metodos de jquery
   $(selector).find(".input").on("change",(ev)=>{
     let $input = $(ev.target)
 
-    let $next_input = $input.next()
+    let $next_step = $input.parent().next(".step")
 
-    enfocar_siguiente_input($next_input)
+    if($next_step.length > 0){
+      siguiente($next_step)
+    }else{
+      validar_formulario()
+    }
 
-    console.log($el)
-
-    console.log("cambie de valor")
   })
   //Helpers
 
   function validar_formulario(){
+    if(es_valido_formulario()){
 
+    }else{
+      let $fieldset_invalido = $(selector).find(".input:invalid").first().parent()
+      siguiente($fieldset_invalido)
+    }
   }
 
   function es_valido_formulario(){
-
+    return document.querySelector(selector).checkValidity()
   }
 
-  function enfocar_siguiente_input($next_input){
-    $next_input.focus()
+  function siguiente($next_step){
+    $(".step.active").removeClass("active")
+    $next_step.addClass("active")
+    $next_step.find(".input").focus()
+
+    // $next_input.focus()
   }
 
 })()
